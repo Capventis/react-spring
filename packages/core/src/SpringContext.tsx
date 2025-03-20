065de58134a8edf330a2af674ea3d1da820e0466
kierancap <kieran.hughes@capventis.com>
2025-03-20 14:19:20 +0000

    feat: Refactored SpringContext

--- packages/core/src/SpringContext.tsx
+++ packages/core/src/SpringContext.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { useContext, PropsWithChildren } from 'react'
-import { useMemoOne } from '@react-spring/shared'
 
 /**
  * This context affects all new and existing `SpringValue` objects
@@ -13,54 +12,29 @@ export interface SpringContext {
   immediate?: boolean
 }
 
-export const SpringContext = makeRenderableContext<
-  SpringContext,
-  PropsWithChildren<SpringContext>
->(
-  Context =>
-    ({ children, ...props }) => {
-      const inherited = useContext(Context)
+export const SpringContext = React.createContext<SpringContext>({
+  pause: false,
+  immediate: false,
+})
 
-      // Inherited values are dominant when truthy.
-      const pause = props.pause || !!inherited.pause
-      const immediate = props.immediate || !!inherited.immediate
+export const SpringContextProvider = ({
+  children,
+  ...props
+}: PropsWithChildren<SpringContext>) => {
+  const inherited = useContext(SpringContext)
 
-      // Memoize the context to avoid unwanted renders.
-      props = useMemoOne(() => ({ pause, immediate }), [pause, immediate])
+  // Inherited values are dominant when truthy.
+  const pause = props.pause ?? inherited.pause ?? false
+  const immediate = props.immediate ?? inherited.immediate ?? false
 
-      return <Context.Provider value={props}>{children}</Context.Provider>
-    },
-  {} as SpringContext
-)
-
-interface RenderableContext<T, P> extends React.ProviderExoticComponent<P> {
-  Provider: RenderableContext<T, P>
-  Consumer: React.Consumer<T>
-  displayName?: string
-}
-
-/** Make the `target` compatible with `useContext` */
-function makeRenderableContext<T, P>(
-  target: (context: React.Context<T>) => React.FunctionComponent<P>,
-  init: T
-): RenderableContext<T, P> {
-  let context = React.createContext(init)
-  context = Object.assign(target(context), context)
-
-  // https://github.com/facebook/react/pull/28226
-  if ('_context' in context.Provider) {
-    context.Provider._context = context
-  } else {
-    // @ts-ignore React 18 types disallow this
-    context.Provider = context
-  }
-
-  if ('_context' in context.Consumer) {
-    context.Consumer._context = context
-  } else {
-    // @ts-expect-error
-    context.Consumer = context
-  }
-
-  return context as unknown as RenderableContext<T, P>
+  // Memoize the context to avoid unwanted renders.
+  const contextValue = React.useMemo(
+    () => ({ pause, immediate }),
+    [pause, immediate]
+  )
+  return (
+    <SpringContext.Provider value={contextValue}>
+      {children}
+    </SpringContext.Provider>
+  )
 }
